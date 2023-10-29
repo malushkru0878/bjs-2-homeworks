de3ab2c9c976e067031c3bd370be3393fcfefd9d
malushkru0878 <malushkru08@mail.ru>
2023-10-29 18:39:43 +0300

    HW arrays added

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
+    let result = true;
+    if (arr1.length != arr2.length) {
+      result = false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] != arr2[i]) {
+        result = false;
+      }
+    }
+    return result;
+  }
   
-}
-
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+  function advancedFilter(arr) {
+    let resultArr = arr.filter(item => item > 0).filter(item => item % 3 == 0).map(item => item *= 10);
+    return resultArr;
+  }
