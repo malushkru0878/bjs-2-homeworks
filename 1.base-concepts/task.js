f55e7f4941276ca66872973b5f56f4bf674edac9
malushkru0878 <malushkru08@mail.ru>
2023-10-31 21:46:51 +0300

    HW functions added

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,34 +1,45 @@
-"use strict";
+"use strict"
 
 function solveEquation(a, b, c) {
-  let arr = [];
-  let disc = b * b - 4 * a * c;
-  if (disc > 0) {
-    arr.push((-b + Math.sqrt(disc)) / 2 / a);
-    arr.push((-b - Math.sqrt(disc)) / 2 / a);
-  } else if (disc == 0) {
-    arr.push(-b / 2 / a);
+  let arr;
+  let discriminant = b ** 2 - 4 * a * c;
+  if (discriminant === 0) {
+    let root = -b / ( 2 * a );
+    arr = [];
+    arr.push(+root.toFixed(2));
+  } else if (discriminant > 0) {
+    let root1 = ( -b + Math.sqrt(discriminant) ) / ( 2 * a );
+    let root2 = ( -b - Math.sqrt(discriminant) ) / ( 2 * a );
+    arr = [];
+    arr.push(+root1.toFixed(2), +root2.toFixed(2));
+  } else {
+    arr = [];
   }
-  return arr;
+  return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  percent = parseFloat(percent);
-  contribution = parseInt(contribution);
-  amount = parseInt(amount);
-
-  //todo added validate
-
-  let endDate = new Date(date);
-  let nowDate = new Date(Date.now);
-
-  //todo fix NaN
-  let totalMonth = (endDate.getFullYear - nowDate.getFullYear) * 12 + endDate.getMonth - nowDate.getMonth;
-
-  let percentPerMonth = percent / 12;
-
-  let amountPerMonth = amount * (percentPerMonth + (percentPerMonth / (Math.pow(1 + percentPerMonth, totalMonth))));
-  let totalAmount = (amountPerMonth * totalMonth).toFixed(2);
+  let totalAmount;
+  percent = +percent;
+  contribution = +contribution;
+  amount = +amount;
+  date = +date;
+  if (typeof percent === `string`) {
+    totalAmount = `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+  } else if (typeof contribution === `string`) {
+    totalAmount = `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`;
+  } else if (typeof amount === `string`) {
+    totalAmount = `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
+  } else if (typeof date === `string`) {
+    totalAmount = `Параметр "Сроки ипотеки" содержит неправильное значение ${date}`;
+  };
+  let lianAmount = amount - contribution;
+  let timeNow = new Date();
+  let loanTerm = date - timeNow;
+  loanTerm = loanTerm / 1000 / 60 / 60 / 24 / 30;
+  let monthlyPartPercent = (percent / 100) / 12;
+  let monthFee = lianAmount * (monthlyPartPercent + (monthlyPartPercent / (((1 + monthlyPartPercent) ** loanTerm) - 1)));
+  totalAmount = +(monthFee * loanTerm).toFixed(2);
   console.log(totalAmount);
   return totalAmount;
 }
